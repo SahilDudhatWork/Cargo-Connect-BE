fec92ddabd983fe71f675dc5926153ef307c4833
sahil dudhat <sahildudhat03@gmail.com>
2024-08-27 15:32:06 +0530

    solve issue

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,26 +1,28 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 
 // Set up storage engine
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Destination folder for uploads
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname}`); // Ensure unique filenames
-  }
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
 });
 
-// File filter to allow only image types
+// File filter to allow images and document types
 const fileFilter = (req, file, cb) => {
-  const allowedTypes = /jpeg|jpg|png|gif/;
-  const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+  const allowedTypes = /jpeg|jpg|png|gif|pdf|doc|docx|xls|xlsx|txt/;
+  const extname = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
   const mimetype = allowedTypes.test(file.mimetype);
 
   if (mimetype && extname) {
     return cb(null, true);
   } else {
-    cb(new Error('Only images are allowed'));
+    cb(new Error("Only images and document files are allowed"));
   }
 };
 
@@ -28,7 +30,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
-  limits: { fileSize: 20 * 1024 * 1024 } // 20 MB limit
+  limits: { fileSize: 20 * 1024 * 1024 }, // 20 MB limit
 });
 
 module.exports = upload;
