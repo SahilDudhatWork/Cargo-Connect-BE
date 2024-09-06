ff8e7a593657f6f409662d007efb125528fa06ca
sahil dudhat <sahildudhat03@gmail.com>
2024-09-06 10:25:05 +0530

    type Of Service

--- routes/user/service.js
+++ routes/user/service.js
@@ -1,8 +1,15 @@
 const { Router } = require("express");
 const { fetchService } = require("../../controller/user/service/fetchService");
-
+const {
+  typeOfService,
+} = require("../../controller/user/service/typeOfService");
+const {
+  userPermission,
+} = require("../../middleware/permission/userPermission");
 const router = Router();
 
+router.get("/typeOfService", typeOfService);
+router.use(userPermission);
 router.get("/", fetchService);
 
 module.exports = router;
