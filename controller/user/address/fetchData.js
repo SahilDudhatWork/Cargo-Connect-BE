4a5897ecedc252862a1760ee7f0a253784f3ccce
sahil dudhat <sahildudhat03@gmail.com>
2024-09-12 12:57:17 +0530

    change swagger & address response

--- controller/user/address/fetchData.js
+++ controller/user/address/fetchData.js
@@ -2,7 +2,6 @@ const Address = require("../../../model/user/address");
 const { handleException } = require("../../../helper/exception");
 const Response = require("../../../helper/response");
 const { ObjectId } = require("mongoose").Types;
-const { paginationResponse } = require("../../../utils/paginationFormate");
 const {
   STATUS_CODE,
   ERROR_MSGS,
@@ -12,51 +11,20 @@ const {
 const fetchData = async (req, res) => {
   let { logger, userId, query } = req;
   try {
-    let { page, limit, sortBy } = query;
-
-    sortBy = sortBy === "recent" ? { createdAt: -1 } : { createdAt: 1 };
-
-    offset = page || 1;
-    limit = limit || 10;
-    const skip = limit * (offset - 1);
     const getData = await Address.aggregate([
       { $match: { userId: new ObjectId(userId) } },
-      { $sort: sortBy },
-      {
-        $facet: {
-          paginatedResult: [
-            { $skip: skip },
-            { $limit: parseInt(limit) },
-            {
-              $project: {
-                __v: 0,
-                password: 0,
-                forgotPassword: 0,
-                token: 0,
-              },
-            },
-          ],
-          totalCount: [{ $count: "count" }],
-        },
-      },
     ]);
 
-    const result = getData[0];
-    let response = await paginationResponse(req, res, offset, limit, result);
-
-    const statusCode =
-      response.response.length > 0 ? STATUS_CODE.OK : STATUS_CODE.OK;
+    const statusCode = getData.length > 0 ? STATUS_CODE.OK : STATUS_CODE.OK;
     const message =
-      response.response.length > 0
-        ? INFO_MSGS.SUCCESS
-        : ERROR_MSGS.DATA_NOT_FOUND;
+      getData.length > 0 ? INFO_MSGS.SUCCESS : ERROR_MSGS.DATA_NOT_FOUND;
 
     return Response[statusCode === STATUS_CODE.OK ? "success" : "error"]({
       req,
       res,
       status: statusCode,
       msg: message,
-      data: response,
+      data: getData,
     });
   } catch (error) {
     console.error("error-->", error);
