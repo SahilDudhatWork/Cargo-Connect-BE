257c971933b2481d94e98235c8c62c534c16bd0c
sahil dudhat <sahildudhat03@gmail.com>
2024-09-30 18:14:40 +0530

    price module / cronejob

--- controller/carrier/vehicle/fetchTransitInfo.js
+++ controller/carrier/vehicle/fetchTransitInfo.js
@@ -1,4 +1,5 @@
 const TransitInfo = require("../../../model/admin/transitInfo");
+const SpecialRequirements = require("../../../model/common/specialRequirements");
 const { handleException } = require("../../../helper/exception");
 const Response = require("../../../helper/response");
 const {
@@ -11,16 +12,27 @@ const fetchTransitInfo = async (req, res) => {
   let { logger } = req;
   try {
     let getData = await TransitInfo.findOne();
+    const getSpecialRequirements = await SpecialRequirements.find(
+      {},
+      { post_bridge: 1 }
+    );
 
-    const statusCode = getData ? STATUS_CODE.OK : STATUS_CODE.OK;
-    const message = getData ? INFO_MSGS.SUCCESS : ERROR_MSGS.DATA_NOT_FOUND;
+    const newData = {
+      modeOfTransportation: getData.modeOfTransportation,
+      typeOfService: getData.typeOfService,
+      typeOfTransportation: getData.typeOfTransportation,
+      port_BridgeOfCrossing: getSpecialRequirements,
+    };
+
+    const statusCode = newData ? STATUS_CODE.OK : STATUS_CODE.OK;
+    const message = newData ? INFO_MSGS.SUCCESS : ERROR_MSGS.DATA_NOT_FOUND;
 
     return Response[statusCode === STATUS_CODE.OK ? "success" : "error"]({
       req,
       res,
       status: statusCode,
       msg: message,
-      data: getData,
+      data: newData,
     });
   } catch (error) {
     console.error("error-->", error);
