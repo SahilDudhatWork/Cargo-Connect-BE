b37f4e574233c83e6e8cb3e288135c014aab0cf7
sahil dudhat <sahildudhat03@gmail.com>
2024-10-01 10:22:26 +0530

    swagger update

--- routes/user/specialRequirements.js
+++ routes/user/specialRequirements.js
@@ -1,11 +1,9 @@
 const { Router } = require("express");
 const router = Router();
-const { getAll } = require("../../controller/user/specialRequirements/getAll");
 const {
   getDetails,
 } = require("../../controller/user/specialRequirements/getDetails");
 
-router.get("/", getAll);
 router.get("/:id", getDetails);
 
 module.exports = router;
