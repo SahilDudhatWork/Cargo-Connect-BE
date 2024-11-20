8dd9765ca97f0af2c71cc0cacde5928df5bb3431
sahil dudhat <sahildudhat03@gmail.com>
2024-11-20 12:50:39 +0530

    fetch sigle guideline

---
+++ controller/common/guidelinePages/fetchSingleGuideline.js
@@ -0,0 +1,36 @@
+const GuidelinePages = require("../../../model/common/guidelinePages");
+const { handleException } = require("../../../helper/exception");
+const Response = require("../../../helper/response");
+const {
+  STATUS_CODE,
+  ERROR_MSGS,
+  INFO_MSGS,
+} = require("../../../helper/constant");
+
+const fetchSingleGuideline = async (req, res) => {
+  let { logger, params } = req;
+  try {
+    const { type, title } = params;
+    const role = type[0].toUpperCase() + type.slice(1).toLowerCase();
+
+    let getData = await GuidelinePages.findOne({ role ,title});
+
+    const statusCode = getData ? STATUS_CODE.OK : STATUS_CODE.OK;
+    const message = getData ? INFO_MSGS.SUCCESS : ERROR_MSGS.DATA_NOT_FOUND;
+
+    return Response[statusCode === STATUS_CODE.OK ? "success" : "error"]({
+      req,
+      res,
+      status: statusCode,
+      msg: message,
+      data: getData,
+    });
+  } catch (error) {
+    console.error("error-->", error);
+    return handleException(logger, res, error);
+  }
+};
+
+module.exports = {
+  fetchSingleGuideline,
+};
--- routes/common/guidelinePages.js
+++ routes/common/guidelinePages.js
@@ -2,8 +2,12 @@ const { Router } = require("express");
 const {
   fetchGuidelinePages,
 } = require("../../controller/common/guidelinePages/fetchGuidelinePages");
+const {
+  fetchSingleGuideline,
+} = require("../../controller/common/guidelinePages/fetchSingleGuideline");
 const router = Router();
 
 router.get("/:type", fetchGuidelinePages);
+router.get("/:type/:title", fetchSingleGuideline);
 
 module.exports = router;
