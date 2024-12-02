018b88ae639cc99b46be57f814f4d9939091ff63
sahil dudhat <sahildudhat03@gmail.com>
2024-12-02 10:07:00 +0530

    fix issue in admin checkPermissions

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const router = Router();
 const { logIn } = require("../../controller/admin/auth/logIn");
+const { signUp } = require("../../controller/admin/auth/signUp");
 const {
   checkPermissions,
 } = require("../../controller/admin/auth/checkPermissions");
+
 const { refreshToken } = require("../../controller/admin/auth/refreshToken");
 const {
   validateEmailAndPassword,
@@ -13,6 +15,8 @@ const { adminAuth } = require("../../middleware/authToken/adminAuth");
 
 router.post("/logIn", validateEmailAndPassword, logIn);
 router.post("/token", refreshAuth, refreshToken);
+
+// router.post("/signUp", validateEmailAndPassword, signUp);
 router.get("/checkPermissions", adminAuth, checkPermissions);
 
 module.exports = router;
